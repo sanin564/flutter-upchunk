88cc4a93e9e28139f4d673dc2caedaec115b902d
sanin564 <sanin@pietechsolution.com>
2025-09-29 10:26:09 +0530

    don't use onErorr directly

--- lib/src/up_chunk.dart
+++ lib/src/up_chunk.dart
@@ -71,7 +71,7 @@ class UpChunk {
     _dio = Dio(
       BaseOptions(
         connectTimeout: const Duration(seconds: 10),
-        receiveTimeout: const Duration(seconds: 5),
+        receiveTimeout: const Duration(seconds: 10),
         validateStatus: (_) => true,
         followRedirects: false,
       ),
@@ -183,7 +183,7 @@ class UpChunk {
 
   void cancel() {
     dispose();
-    onError?.call(CancelledByUser(), StackTrace.current);
+    _handleError(CancelledByUser(), StackTrace.current);
   }
 }
 
